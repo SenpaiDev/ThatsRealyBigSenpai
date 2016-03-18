cea954bc6c9b0d4cb8e4aab48d5850fe8f3e8a75
SenpaiDev <frostbyte@ohyessenpaipleaseputit.in>
2016-03-18 19:31:44 +0100

    ssASas

--- ThatsRealyBigSenpai/ThatsRealyBigSenpai/Oniichan.cs
+++ ThatsRealyBigSenpai/ThatsRealyBigSenpai/Oniichan.cs
@@ -4,22 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-
+using TheForest.Utils;
 
 namespace ThatsRealyBigSenpai
 {
     public class Oniichan : MonoBehaviour
     {
+        private static int memeW = 500;
+        private static int memeH = 300;
+        private static float screenH = Screen.height / 2 - memeH / 2;
+        private static float screenW = Screen.width / 2 - memeW / 2;
+        private bool Menutog = false;
+        private GUIStyle CatBG = new GUIStyle();
+        private Texture2D CatBGColor = new Texture2D(1, 1, TextureFormat.ARGB32, false);
         void Start(){
-
+            CatBGColor.SetPixels(new Color[] {new Color(32,32,32,250)});
         }
 
         void OnGUI(){
-           
-        }
+            if (Menutog){
+                CatBG = new GUIStyle("box");
+                CatBG.normal.background = CatBGColor;
+                GUI.Box(new Rect(screenW, screenH, memeW, memeH), "", CatBG);
+                GUILayout.BeginArea(new Rect(screenW, screenH, memeW, memeH));
+                GUILayout.EndArea();
+
+            }
+        } // >.<
 
         void Update(){
-
+            if (UnityEngine.Input.GetKeyDown(KeyCode.G)){
+                Menutog = !Menutog;
+            }
         }
     }
 }
