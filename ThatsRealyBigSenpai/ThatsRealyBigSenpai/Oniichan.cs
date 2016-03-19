211394838408b8866a4820e462dbaffe9dcab957
SenpaiDev <frostbyte@ohyessenpaipleaseputit.in>
2016-03-19 13:37:04 +0100

    dad

--- ThatsRealyBigSenpai/ThatsRealyBigSenpai/Oniichan.cs
+++ ThatsRealyBigSenpai/ThatsRealyBigSenpai/Oniichan.cs
@@ -12,29 +12,57 @@ namespace ThatsRealyBigSenpai
     {
         private static int memeW = 500;
         private static int memeH = 300;
-        private static float screenH = Screen.height / 2 - memeH / 2;
-        private static float screenW = Screen.width / 2 - memeW / 2;
+        private static int screenH = Screen.height / 2 - memeH / 2;
+        private static int screenW = Screen.width / 2 - memeW / 2;
         private bool Menutog = false;
-        private GUIStyle CatBG = new GUIStyle();
-        private Texture2D CatBGColor = new Texture2D(1, 1, TextureFormat.ARGB32, false);
-        void Start(){
-            CatBGColor.SetPixels(new Color[] {new Color(32,32,32,250)});
+        public GUIStyle CatBG = new GUIStyle();
+        public Texture2D CatBGColor = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+        public GUIStyle SelBG = new GUIStyle();
+        public Texture2D SelBGColor = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+        void Start() {
+            CatBGColor.SetPixels(new Color[] { new Color(12, 12, 12, 200) });
+            SelBGColor.SetPixels(new Color[] { new Color(200, 52, 52, 52) });
         }
 
-        void OnGUI(){
-            if (Menutog){
-                CatBG = new GUIStyle("box");
+
+
+        void OnGUI() {
+            if (Menutog) {
+
+
+                CatBG = new GUIStyle("button");
                 CatBG.normal.background = CatBGColor;
-                GUI.Box(new Rect(screenW, screenH, memeW, memeH), "", CatBG);
-                GUILayout.BeginArea(new Rect(screenW, screenH, memeW, memeH));
+                CatBG.hover.background = CatBGColor;
+                CatBG.active.background = SelBGColor;
+
+                GUILayout.BeginArea(new Rect(screenW, screenH, 100, memeH));
+                if (GUILayout.Button("Player", CatBG, GUILayout.Height(60))) { }
+                GUILayout.Space(-3);
+                if (GUILayout.Button("Items", CatBG, GUILayout.Height(60))) { }
+                GUILayout.Space(-3);
+
+                if (GUILayout.Button("Misc", CatBG, GUILayout.Height(60))) { }
+                GUILayout.Space(-3);
+
+                if (GUILayout.Button("Online", CatBG, GUILayout.Height(60))) { }
+                GUILayout.Space(-3);
+                if (GUILayout.Button("Options", CatBG, GUILayout.Height(60))) { }
                 GUILayout.EndArea();
 
-            }
-        } // >.<
 
+            }
+
+
+        }
         void Update(){
             if (UnityEngine.Input.GetKeyDown(KeyCode.G)){
                 Menutog = !Menutog;
+                if (Menutog)
+                {
+                    LocalPlayer.FpCharacter.LockView(true);
+                } else { 
+                    LocalPlayer.FpCharacter.UnLockView();
+              }
             }
         }
     }
---
+++ ThatsRealyBigSenpai/ThatsRealyBigSenpai/dildo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ThatsRealyBigSenpai
+{
+    public class dildo : MonoBehaviour{
+        public static GameObject dd;
+        
+        public static void meme(){
+            if (dd == null){
+                dd = new GameObject();
+                dd.AddComponent<Oniichan>();
+                UnityEngine.Object.DontDestroyOnLoad(dd);
+            }
+        }
+    }
+}
